1958080edf0c00f32e84222e996c5bf13575d3dc
AQodS <qoddri@gmail.com>
2025-08-21 19:09:36 +0700

    static and dynamic routing

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# static route
 @app.route("/")
 def index():
     return "<h1>Hello, World!<h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+@app.route("/contact")
+def contact():
+    return "<h1>Contact Page</h1>"
+
+# dynamic route
+@app.route("/profile", defaults={"_route": "home", "username": "Guess"}) # default parameter
+@app.route("/profile/<string:username>", defaults={"_route": "profile"})
+def profile_name(username, _route):
+    if _route == "home":
+        return f"<h1>Profile Home Page of {username}</h1>"
+    elif _route == "profile":
+        return f"<h1>Profile Page of {username}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
